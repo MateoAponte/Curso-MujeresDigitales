ffca8a3bb17e41e1912ad8b166c561ce29d06592
Mateo <mateo2aponte@hotmail.com>
2024-10-08 14:53:28 -0500

    feat: Add semana 8-9 folder

--- talleres/taller-4/advice-generator-front/src/components/AdviceChildren.jsx
+++ talleres/taller-4/advice-generator-front/src/components/AdviceChildren.jsx
@@ -3,13 +3,14 @@ import { Advice } from './Advice';
 import AdviceContext from '../context/AdviceContext';
 
 export const AdviceChildren = () => {
-  const { slip, dispatch, loading } = useContext(AdviceContext);
+  const { slip, dispatch, loading, fetchData } = useContext(AdviceContext);
 
   return (
     <Advice
       id={slip.id}
       description={slip.advice}
       dispatch={dispatch}
+      fetchData={fetchData}
       loading={loading}
     />
   );
